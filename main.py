3b3e74639d90dde1e14f563d97f9b1e0d296d395
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2023-12-15 18:27:00 +0330

    Adding register and login functions

---
+++ main.py
@@ -0,0 +1,61 @@
+# Need to install ( pip install flask )
+from flask import Flask, render_template, request, redirect, url_for, session
+
+# Need to install ( pip install flask flask-socketio )
+from flask_socketio import SocketIO, emit
+
+# Standard python library
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Creating a web application.
+app = Flask(__name__)
+
+# Creating communication with the server for messages in real-time.
+socketio = SocketIO(app)
+
+users = {}
+
+
+# ---------- Functions ----------
+
+# To specify the link to register and authorized methods.
+@app.route("/register", methods=['Get', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if username in users:
+            error = "This username already exists!"
+        elif password != confirm_password:
+            error = "Passwords do not match!"
+        else:
+            users[username] = {
+                'username': username,
+                'password': generate_password_hash(password)
+            }
+            print(users)
+            return redirect(url_for('login'))
+        print(error)
+        return render_template('register.html', error=error)
+    return render_template('register.html')
+
+
+# To specify the link to login and authorized methods.
+@app.route("/login", methods=['Get', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and check_password_hash(users[username]['password'], password):
+            session['username'] = username
+            return redirect(url_for('index'))
+        error = "The username or password is incorrect!"
+        print("error!")
+        return render_template('login.html', error=error)
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = '8MLn5jxy7c3ouKrk4DCe5ALMrg29'
+    socketio.run(app, debug=True)
